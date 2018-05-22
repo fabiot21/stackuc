ace770f2995bcd1700cf68027cae6fefeed254a7
Jaime Y <jsyanez@uc.cl>
2018-05-21 20:18:56 -0400

    Agregados comentarios

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { base } from './Firebase';
 import { Label, Header,Form,Button, Comment,Loader, Divider, Rating } from 'semantic-ui-react';
 import DefaultAvatar from '../assets/default-avatar.png'
+import { auth } from './Firebase'
+
 const ReactMarkdown = require('react-markdown')
 
 class Question extends Component {
@@ -22,6 +24,76 @@ class Question extends Component {
       this.setState({questionData: data})
     }
       });
+      this.fetchAnswers()
+  }
+
+  fetchAnswers = () =>
+      base.bindToState('answers/' + this.state.questionId, {
+      context: this,
+      state: 'answersData',
+      asArray: false
+      });
+    /*base.fetch('answers/' + this.state.questionId,{
+          context: this,
+          asArray: false,  //No pido array porque necesito el id de cada comentario (para los likes)
+          then(data){
+            this.setState({answersData: data})
+          }
+        });*/
+
+
+  handleChange = (e, {name,value}) => this.setState({'answer' : value})
+
+  handleSubmit = () => {    base.push('answers/'+this.state.questionId, {
+        data: {
+          userEmail: auth.currentUser.email,
+          content: this.state.answer,
+          votes: 0,
+          points: 0,
+        }
+      })
+      document.getElementById('field_answer').value = ''
+        };
+
+  renderCommentGroup = () => {
+    if (!this.state.answersData) {
+      return (
+        <Loader active inline='centered' />
+      )
+    }
+    const comments = Object.keys(this.state.answersData).map((key, index) => (
+      this.renderComment(this.state.answersData[key])
+    ));
+
+    return(
+    <Comment.Group>
+      <Header as='h2' dividing>Respuestas</Header>
+      {comments}
+      <Form reply onSubmit={this.handleSubmit}>
+        <Form.TextArea id='field_answer' name='answer' onChange= {this.handleChange}/>
+        <Form.Button content='Agrega una respuesta' labelPosition='left' icon='edit' primary />
+      </Form>
+
+    </Comment.Group>
+    )
+  }
+
+  renderComment = (comment) => {
+      console.log(comment)
+      return(
+        <Comment>
+          <Comment.Avatar src={DefaultAvatar}/>
+          <Comment.Content>
+            <Comment.Author as='a'>{comment.userEmail}</Comment.Author>
+            <Comment.Metadata>
+              <Rating icon='star' defaultRating={0} maxRating={5} />
+            </Comment.Metadata>
+            <Comment.Text>
+                 <ReactMarkdown source={comment.content}/>
+            </Comment.Text>
+          </Comment.Content>
+        </Comment>
+      )
   }
 
   render() {
@@ -45,39 +117,7 @@ class Question extends Component {
         <Divider />
           <ReactMarkdown source={this.state.questionData.content}/>
         <Divider />
-        <Comment.Group>
-          <Header as='h2' dividing>Respuestas</Header>
-          <Comment>
-            <Comment.Avatar src={DefaultAvatar}/>
-            <Comment.Content>
-              <Comment.Author as='a'>El choro</Comment.Author>
-              <Comment.Metadata>
-                <Rating icon='star' defaultRating={0} maxRating={5} />
-              </Comment.Metadata>
-              <Comment.Text>
-                   <ReactMarkdown source={"holi x2"}/>
-              </Comment.Text>
-            </Comment.Content>
-          </Comment>
-          <Comment>
-            <Comment.Avatar src={DefaultAvatar}/>
-            <Comment.Content>
-              <Comment.Author as='a'>El pulento</Comment.Author>
-              <Comment.Metadata>
-              <Rating icon='star' defaultRating={0} maxRating={5} />
-              </Comment.Metadata>
-              <Comment.Text>
-                <ReactMarkdown source={"holi"}/>
-              </Comment.Text>
-            </Comment.Content>
-          </Comment>
-
-          <Form reply>
-            <Form.TextArea />
-            <Button content='Add Reply' labelPosition='left' icon='edit' primary />
-          </Form>
-
-        </Comment.Group>
+        {this.renderCommentGroup()}
 
 
 
