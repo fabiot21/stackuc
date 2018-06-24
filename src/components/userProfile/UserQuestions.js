69d03c6f7c5c8d9c83b60776269803bc5243f4a0
Jaime Y <jsyanez@uc.cl>
2018-06-24 14:29:21 -0400

    Agregadas preguntas en perfil

---
+++ src/components/userProfile/UserQuestions.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { Loader, Feed, Icon } from 'semantic-ui-react'
+import { base } from '../Firebase'
+import { connect } from 'react-redux'
+
+class UserQuestions extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {'questions': [] }
+    this.fetchQuestionsFromFirebase = this.fetchQuestionsFromFirebase.bind(this)
+  }
+
+  componentDidMount(){
+    this.fetchQuestionsFromFirebase()
+  }
+
+  fetchQuestionsFromFirebase(){
+    base.fetch('questions/', {
+      context: this,
+      asArray: true,
+      queries: {
+        orderByChild: 'author',
+        equalTo: this.props.currentUser.userEmail
+        },
+      then(data){
+        this.setState({'questions': data})
+      }
+    });
+  }
+
+  renderQuestionFeed(question){
+    const questionSummary = ("Preguntaste " +question.title + "")
+    return(
+      <Feed.Event
+        icon='question circle'
+        date='TODO: agregar fecha a todo'
+        summary= {questionSummary}
+      />
+    )
+  }
+
+  renderAllQuestions(){
+    const htmlData = this.state.questions.map( (question) => {
+      return this.renderQuestionFeed(question)
+    } )
+    if(htmlData.length === 0 ){
+      return  <Loader active>Cargando</Loader>
+    }
+    return(
+      <Feed>
+        {htmlData}
+      </Feed>
+    )
+  }
+
+  render(){
+    return(
+      <div>
+        {this.renderAllQuestions()}
+      </div>
+    )
+  }
+}
+
+
+function mapStateToProps({ currentUser }){
+  return {
+    currentUser
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { })(UserQuestions)
